e49698d890396be1268feb424775994d085a8a2f
MajoGonzalez <majogonzalez4@gmail.com>
2020-06-22 16:54:31 -0500

    Diseño de Interfaz

--- app/src/main/java/com/mariagonzalez/firebaseapp1/FragmentSearch.java
+++ app/src/main/java/com/mariagonzalez/firebaseapp1/FragmentSearch.java
@@ -7,58 +7,50 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentSearch#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+//Generar la lista, quise hacer un ejemplo, sin embargo tuve un problema con el adapter :D
+//Para poder mostrar mis datos
+
 public class FragmentSearch extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    SearchView mSearchView;
+    ListView mList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
 
     public FragmentSearch() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentSearch.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static FragmentSearch newInstance(String param1, String param2) {
         FragmentSearch fragment = new FragmentSearch();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        mSearchView = (SearchView) view.findViewById(R.id.searchView);
+        mList = (ListView) view.findViewById(R.id.myList);
+
+        return view;   }
 }
